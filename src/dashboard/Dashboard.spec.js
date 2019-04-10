29d5e2412544327b057a92c4e7b94629194aec2b
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-10 12:32:00 +0100

    MVP: Initialize tests for Dashboard and Display components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+
+describe('Dashboard component', () => {
+  it('should render without crashing', () => {
+    const wrap = rtl.render(<Dashboard />);
+    expect(wrap).toBeTruthy();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
+
+describe('Display component', () => {
+  it('should render without crashing', () => {
+    const wrap = rtl.render(<Display />);
+    expect(wrap).toBeTruthy();
+  });
+});
