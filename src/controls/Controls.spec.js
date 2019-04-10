8af2a1a65a8284ab1c3e6aea9c9e823471126de0
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-10 12:31:08 +0100

    MVP: Initialize controls test and add snapshot

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe('Controls component', () => {
+  it('should render without crashing', () => {
+    const wrap = rtl.render(<Controls />);
+    expect(wrap).toBeTruthy();
+    expect(wrap.asFragment()).toMatchSnapshot();
+  });
+});
