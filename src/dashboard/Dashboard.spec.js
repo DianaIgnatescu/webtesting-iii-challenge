0e48a5be7e98d528b4d626f908f38fef6c1515af
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-10 14:49:19 +0100

    MVP: Write tests for Dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -5,9 +5,41 @@ import 'jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
 
+afterEach(rtl.cleanup);
+
 describe('Dashboard component', () => {
   it('should render without crashing', () => {
     const wrap = rtl.render(<Dashboard />);
     expect(wrap).toBeTruthy();
   });
+  it('should be able to toggle between open and closed when clicked', () => {
+    const wrap = rtl.render(<Dashboard />);
+    const closedButton = wrap.getByTestId('closeButton');
+
+    rtl.fireEvent.click(closedButton);
+    expect(wrap.getByText('Open Gate'));
+
+    rtl.fireEvent.click(closedButton);
+    expect(wrap.getByText('Close Gate'));
+  });
+  it('should be able to toggle between locked and unlocked when clicked', () => {
+    const wrap = rtl.render(<Dashboard />);
+    const lockedButton = wrap.getByTestId('lockButton');
+    const closedButton = wrap.getByTestId('closeButton');
+
+    rtl.fireEvent.click(closedButton);
+    rtl.fireEvent.click(lockedButton);
+    expect(wrap.getByText('Unlock Gate'));
+
+    rtl.fireEvent.click(lockedButton);
+    expect(wrap.getByText('Lock Gate'));
+  });
+  it('should not be able to render the locked gate if gate is open', () => {
+    const wrap = rtl.render(<Dashboard />);
+    const lockedButton = wrap.getByTestId('lockButton');
+
+    rtl.fireEvent.click(lockedButton);
+    expect(wrap.queryByText('Unlock Gate')).toBeFalsy();
+  })
+
 });
